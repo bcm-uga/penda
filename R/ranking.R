4785984b2a1ede78bf3fa19c12405414d15b0ac8
magrichard <mag.richard@orange.fr>
2018-04-17 14:21:48 +0200

    correct detect_zero_value function and vignettes

--- R/ranking.R
+++ R/ranking.R
@@ -30,6 +30,7 @@ detect_zero_value = function(ctrl_data, cancer_data, threshold, min = 0) {
       return(FALSE)
     }
   })
+  print(paste0(sum(values0), " genes have less than ", min, " counts in ", threshold*100, " % of the samples."))
 }
 
 
--- examples/ex_detect_zero_value.R
+++ examples/ex_detect_zero_value.R
@@ -1,4 +1,6 @@
 # Example for detect_zero_value function
-null_values = detect_zero_value(ctrl_data, simu_data, threshold = 0.8, min = 10)
-ctrl_data = ctrl_data[!null_values,]
-simu_data = simu_data[!null_values,]
+data_ctrl = penda::data_ctrl
+data_case = penda::data_case
+null_values = detect_zero_value(data_ctrl, data_case, threshold = 0.99, min = 10)
+data_ctrl_filtered = data_case[!null_values,]
+data_case_filtered = data_case[!null_values,]
