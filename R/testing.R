edc7f0ba251c4c62ed268c9b181b818a94134d63
Clementine Decamps <clementine.decamps@gmail.com>
2018-04-23 16:26:27 +0200

    New complex simulation, supression of step 0

--- R/testing.R
+++ R/testing.R
@@ -154,30 +154,6 @@ original_test = function(gene, D_U_ctrl, patient_genes, threshold, ctrl_data){
   }
 }
 
-#'step0
-#'
-#' This function makes the list of genes at the extremities of expression distribution.
-#'
-#'@param ctrl_data The matrix with genes expressions in controls for all the patients.
-#'@param patient_genes The vector of genes expressions for one patient.
-#'@param quant The quantile for the expression distribution limit.
-#'
-#'@return This function return a logical vector with TRUE for genes outside of the distribution limit.
-#'
-#'@examples
-#'simulation = ctrl_data[,ncol(ctrl_data)-3 : ncol(ctrl_data)]
-#'simulation = simplified_simulation(simulation, fraction = 0.3, threshold = 60)
-#'ctrl_data = ctrl_data[,-ncol(ctrl_data)-3:ncol(ctrl_data)]
-#'step0(ctrl_data, simulation$simulated_data[,1], quant = 0.03)
-#'
-#'@export
-
-step0 = function (ctrl_data, patient_genes, quant){
-  quantile_gene = apply(ctrl_data, 1, quantile, c(quant,(1-quant)))
-  outsiders = ((patient_genes < quantile_gene[1,]) | (patient_genes > quantile_gene[2,]))
-  return(outsiders)
-}
-
 
 #'patient_test
 #'
@@ -185,74 +161,6 @@ step0 = function (ctrl_data, patient_genes, quant){
 #'
 #'@param ctrl_data A matrix with genes expressions in controls for all the patients.
 #'@param patient_genes A vector with all the genes expressions for one patient.
-#'@param quant_0 The quantile of the expression distribution for the step_0.
-#'@param iterations The maximal number of iterations for the test. If the dysregulation list
-#'no longer changes, iterations are stop before.
-#'@param D_U_ctrl The list of Down and Up-expressed genes matrices in the control.
-#'@param threshold The threshold for regulation_test
-#'@param quant_test The quantile for the test. When D and U lists are void, we use the naive method.
-#'@param factor_test The factor for the test. The limit D will be quant(gene)/factor, and the limit U quant(gene)*factor.
-#'
-#'
-#'@return This function return a list with two vectors :
-#'D, with TRUE for genes down-regulated
-#'U, with TRUE for genes up-regulated.
-#'
-#'@examples
-#'simulation = ctrl_data[,ncol(ctrl_data)-3 : ncol(ctrl_data)]
-#'simulation = simplified_simulation(simulation, fraction = 0.3, threshold = 60)
-#'ctrl_data = ctrl_data[,-ncol(ctrl_data)-3:ncol(ctrl_data)]
-#'D_U_ctrl = find_D_U_ctrl(ctrl_data, quant = 0.001, factor = 4, threshold = 0.99)
-#'test_patient1 = patient_test(ctrl_data, simulation$simulated_data[,1], quant_0 = 0.03, iterations = 10, D_U_ctrl, threshold = 0.2)
-#'
-#'@export
-
-patient_test = function (ctrl_data, patient_genes, quant_0, iterations, D_U_ctrl, threshold, quant_test = 0, factor_test = 1){
-
-  #Suspicious genes with an expression out of bounds are removed of D_U.
-  print ("Step 0")
-  l0 = step0(ctrl_data, patient_genes, quant_0)
-  D_U_ctrl$D = D_U_ctrl$D * !l0
-  D_U_ctrl$U = D_U_ctrl$U * !l0
-  l1 = rep(FALSE, nrow(ctrl_data))
-  l1n1 = rep(FALSE, nrow(ctrl_data))
-  l1n2 = rep(FALSE, nrow(ctrl_data))
-  print("Begining of iterations")
-  #For each iteration
-  for (i in 1:iterations){
-    print(i)
-    #Genes dysregulated at the previous iteration are removed of D_U.
-    D_U_ctrl_tmp = list()
-    D_U_ctrl_tmp$D = D_U_ctrl$D * !abs(l1)
-    D_U_ctrl_tmp$U = D_U_ctrl$U * !abs(l1)
-    #The dysregulation of each genes is compute.
-    l1 = sapply(names(patient_genes), function(gene){
-      expression = regulation_test(gene, D_U_ctrl_tmp, patient_genes, threshold, ctrl_data, quant = quant_test, factor = factor_test)
-      return(expression)
-    })
-    if((sum(l1 == l1n1) == length(l1)) | (sum(l1 == l1n2) == length(l1))){
-      print ("Stabilization of the dysregulated genes list")
-      break
-    } else if (i == iterations) {
-      print ("Maximum iterations without stabilization")
-    } else {
-      l1n2 = l1n1
-      l1n1 = l1
-    }
-  }
-  U_genes = (l1 == 1 | l1n1 == 1)
-  D_genes = (l1 == -1 | l1n1 == -1)
-  return(list(D = D_genes, U = U_genes))
-  gc()
-}
-
-
-#'patient_test_sansl0
-#'
-#' This function makes the test of the dysregulation of all genes for one patient, without the step0.
-#'
-#'@param ctrl_data A matrix with genes expressions in controls for all the patients.
-#'@param patient_genes A vector with all the genes expressions for one patient.
 #'@param iterations The maximal number of iterations for the test. If the dysregulation list
 #'no longer changes, iterations are stop before.
 #'@param D_U_ctrl The list of Down and Up-expressed genes matrices in the control.
@@ -269,13 +177,12 @@ patient_test = function (ctrl_data, patient_genes, quant_0, iterations, D_U_ctrl
 #'simulation = simplified_simulation(simulation, fraction = 0.3, threshold = 60)
 #'ctrl_data = ctrl_data[,-ncol(ctrl_data)-3:ncol(ctrl_data)]
 #'D_U_ctrl = find_D_U_ctrl(ctrl_data, quant = 0.001, factor = 4, threshold = 0.99)
-#'patient_test_sansl0(ctrl_data, simulation$simulated_data, iterations = 10, D_U_ctrl, threshold = 0.2)
+#'test_patient1 = patient_test(ctrl_data, simulation$simulated_data[,1], iterations = 10, D_U_ctrl, threshold = 0.2)
 #'
 #'@export
 
-patient_test_sansl0 = function (ctrl_data, patient_genes, iterations, D_U_ctrl, threshold, quant_test = 0, factor_test = 1){
+patient_test = function (ctrl_data, patient_genes, iterations, D_U_ctrl, threshold, quant_test = 0, factor_test = 1){
 
-  print ("Not step 0")
   l1 = rep(FALSE, nrow(ctrl_data))
   l1n1 = rep(FALSE, nrow(ctrl_data))
   l1n2 = rep(FALSE, nrow(ctrl_data))
