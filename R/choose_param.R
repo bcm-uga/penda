f52da3d6f64f4d605f7a67c508409be7d54e566e
Clementine Decamps <clementine.decamps@gmail.com>
2018-09-26 11:15:32 +0200

    New version of choose_quantile and choose_threshold with select_threshold_param and select_quantile_param

--- R/choose_param.R
+++ R/choose_param.R
@@ -75,7 +75,7 @@ test_multiple_thresholds = function(controls, D_U_list, simulation, threshold_va
 #' Test gene expression variations for multiple thresholds in a serie of samples
 #'
 #' This function makes for each patient the Penda test with different values for the treshold.
-#' After that, it computes FDR and TPR, and print the threshold closest of your goal FDR.
+#' It computes FDR, TPR and FPR for each patient and each threshold value.
 #'
 #'@param controls A matrix with genes expressions in controls for all the patients.
 #'@param D_U_list The list of Down and Up-expressed genes matrices in the control.
@@ -83,18 +83,17 @@ test_multiple_thresholds = function(controls, D_U_list, simulation, threshold_va
 #'no longer changes, iterations are stop before.
 #'@param simulation The simulation with the list of initial data $initial_data and modified data in $simulated_data.
 #'@param threshold_values A vector with different values to test for the threshold.
-#'@param FDR_goal The FDR you would like for this test.
 #'@param quant_test The quantile for the test. When D and U lists are void, we use the naive method.
 #'@param factor_test The factor for the test. The limit D will be quant(gene)/factor, and the limit U quant(gene)*factor.
 #'
-#'@return This function returns a matrix with 4 columns : the patient number, the value of the threshold tested,
-#'the FDR and the TPR of the test.
+#'@return This function returns a matrix with 5 columns : the patient number, the value of the threshold tested,
+#'the FDR, the TPR and the FPR of the test.
 #'
 #'@example examples/ex_choose_threshold.R
 #'
 #'@export
 
-choose_threshold = function(controls, D_U_list, iterations, simulation, threshold_values, FDR_goal = 0.06, quant_test = 0, factor_test = 1){
+choose_threshold = function(controls, D_U_list, iterations, simulation, threshold_values, quant_test = 0, factor_test = 1){
   results = c()
   #If simulation is a matrix, we do the test and compute errors for each patient
   if (is.matrix(simulation$initial_data)) {
@@ -109,7 +108,7 @@ choose_threshold = function(controls, D_U_list, iterations, simulation, threshol
       for(value in 1:ncol(test$D)){
         #Computing of FP, TP, FN, TN
         results_simu = results_simulation(test$D[,value], test$U[,value], simulation_p)
-        #Computing of FDR and TPR
+        #Computing of FDR, TPR and FPR
         FDR = results_simu$FP / (results_simu$TP + results_simu$FP)
         TPR  = results_simu$TP / (results_simu$TP + results_simu$FN)
         FPR = results_simu$FP / (results_simu$TN + results_simu$FP)
@@ -122,7 +121,7 @@ choose_threshold = function(controls, D_U_list, iterations, simulation, threshol
     for(value in 1:ncol(test$D)){
       #Computing of FP, TP, FN, TN
       results_simu = results_simulation(test$D[,value], test$U[,value], simulation)
-      #Computing of FDR and TPR
+      #Computing of FDR, TPR and FPR
       FDR = results_simu$FP / (results_simu$TP + results_simu$FP)
       TPR  = results_simu$TP / (results_simu$TP + results_simu$FN)
       FPR = results_simu$FP / (results_simu$TN + results_simu$FP)
@@ -130,32 +129,6 @@ choose_threshold = function(controls, D_U_list, iterations, simulation, threshol
     }
   }
 
-  #Search the FDR goal.
-  median_t = c()
-  for (value in 1:length(threshold_values)){
-    threshold = threshold_values[value]
-    FDRt = as.numeric(results[results[,2] == threshold & !is.na(results[,3]), 3])
-    median_t = rbind(median_t, c(threshold, median(FDRt)))
-  }
-  small_FDR = median_t[median_t[,2] <= FDR_goal,]
-  if(length(small_FDR) !=0){
-    #If only one FDR under the FDR goal,
-    if (is.vector(small_FDR)){
-      print ("The threshold closest to the FDR is")
-      print (small_FDR[1])
-      print("Which has a median FDR of")
-      print(small_FDR[2])
-    #If more than one FDR under the FDR goal,
-    } else {
-      idx = which(small_FDR[,2] == max(small_FDR[,2]))
-      print ("The threshold closest to the FDR is")
-      print (small_FDR[idx, 1])
-      print("Which has a median FDR of")
-      print(small_FDR[idx, 2])
-    }
-  } else {
-    print ("Your FDR is not reachable, check the results table to choose your threshold.")
-  }
   colnames(results) = c("patient", "threshold", "FDR", "TPR", "FPR")
   return(results)
 }
@@ -165,55 +138,134 @@ choose_threshold = function(controls, D_U_list, iterations, simulation, threshol
 # clementine.decamps@univ-grenoble-alpes.fr
 #
 #---------------------------------------------
+#' Select the better threshold to reach a given FDR.
+#'
+#' This function use the result of choose_threshold to give the parameter which allows to reach
+#' the given FDR. If there are multiple patients, it uses the median
+#'
+#'@param which_threshold The matrix result of choose_threshold, with patient, FDR and threshold.
+#'@param FDR_goal The maximum FDR wanted.
+#'
+#'@return This function returns a list of 2 parameters : the best threshold and the associated FDR.
+#'
+#'@example examples/ex_select_threshold_param.R
+#'
+#'@export
+
+select_threshold_param = function(which_threshold, FDR_goal = 0.05) {
+  median_t = c()
+  which_threshold = apply(which_threshold, 2, as.numeric)
+  threshold_values = which_threshold[,"threshold"]
+
+  for (value in 1:length(threshold_values)){
+    threshold = threshold_values[value]
+    FDRt = as.numeric(which_threshold[which_threshold[,"threshold"] == threshold & !is.na(which_threshold[,"FDR"]), "FDR"])
+    median_t = rbind(median_t, c(threshold, median(FDRt)))
+  }
+
+  small_FDR = median_t[median_t[,2] <= FDR_goal,]
+
+  if(length(small_FDR) != 0){
+    #If only one FDR under the FDR goal,
+    if (is.vector(small_FDR)){
+      print (c("The threshold closest to the FDR is ", small_FDR[1], "which has a median FDR of",
+               small_FDR[2]), quote = FALSE)
+      return(list(threshold = small_FDR[1], FDR = small_FDR[2]))
+
+      #If more than one FDR under the FDR goal,
+    } else {
+      idx = which(small_FDR[,2] == max(small_FDR[,2]))
+      print (c("The threshold closest to the FDR is ", small_FDR[idx, 1]
+               , "which has a median FDR of", small_FDR[idx, 2]), quote = FALSE)
+      return(list(threshold = small_FDR[idx,1], FDR = small_FDR[idx,2]))
+    }
+  } else {
+    print ("Your FDR is not reachable, check the results table to choose your threshold.")
+  }
+}
+
+
+# Authors: Clémentine Decamps, UGA
+# clementine.decamps@univ-grenoble-alpes.fr
+#
+#---------------------------------------------
 #'Test gene expression variations with the quantile test for multiple quantile values in a serie of samples
 #'
-#' This function makes for each patient the quantile test with different values for the quantil.
-#' After that, it computes FDR and TPR, and print the quantile closest of your goal FDR.
+#' This function makes the quantile test for each patient with different values of quantile and factor.
+#' for each set of parameters, it computes FDR, TPR and FPR.
 #'
 #'@param controls A matrix with genes expressions in controls for all the patients.
 #'@param simulation The list of initial data $initial_data and modified data in $simulated_data
-#'@param FDR_goal The FDR you would like for this test.
-#'@param factor The factor for the test The D limit will be quantmin/factor, and the U limit quantmax*factor.
+#'@param factor A vector with different values of factor. The D limit will be quantmin/factor, and the U limit quantmax*factor.
 #'@param quantile_values A vector with different values to test for the quantile.
 #'
-#'@return This function returns a matrix with 3 columns : the value of the quantile tested,
-#'the FDR and the TPR of the test.
+#'@return This function returns a matrix with 5 columns : the value of the factor test,
+#'the value of the quantile tested, the FDR, the TPR and the FPR of the test.
 #'
 #'@example examples/ex_choose_quantile.R
 #'
 #'@export
 
-choose_quantile = function(controls, simulation, FDR_goal, factor = 1.4, quantile_values = c(0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2)){
+choose_quantile = function(controls, simulation, factor_values = c(1, 1.2, 1.4, 1.6, 1.8, 2), quantile_values = c(0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2)){
 
   results = c()
-  print ("Computing results")
-  for (q in 1:length(quantile_values)){
-    test = quantile_test(controls, simulation$simulated_data, quantile_values[q], factor)
-    results_simu = results_simulation(test$D, test$U, simulation)
-    FDR = results_simu$FP / (results_simu$TP + results_simu$FP)
-    TPR  = results_simu$TP / (results_simu$TP + results_simu$FN)
-    FPR = results_simu$FP / (results_simu$TN + results_simu$FP)
-    results = rbind(results, c(quantile_values[q], FDR, TPR, FPR))
+  for (f in factor_values){
+    print(c("Facteur", f))
+    for (q in quantile_values){
+      print(c("Quantile", q))
+      test = quantile_test(controls, simulation$simulated_data, q, f)
+      results_simu = results_simulation(test$D, test$U, simulation)
+      FDR = results_simu$FP / (results_simu$TP + results_simu$FP)
+      TPR  = results_simu$TP / (results_simu$TP + results_simu$FN)
+      FPR = results_simu$FP / (results_simu$TN + results_simu$FP)
+      results = rbind(results, c(f, q, FDR, TPR, FPR))
+    }
   }
+  colnames(results) = c("factor", "quantile", "FDR", "TPR", "FPR")
+  return(results)
+}
 
-  #Compute the small FDR.
-  small_FDR = results[results[,2] <= FDR_goal & !is.na(results[,2]) ,]
+
+
+# Authors: Clémentine Decamps, UGA
+# clementine.decamps@univ-grenoble-alpes.fr
+#
+#---------------------------------------------
+#' Select the better factor and quantile to reach a given FDR.
+#'
+#' This function use the result of choose_quantile to give parameters which allow to reach
+#' the given FDR with the best TPR.
+#'
+#'@param which_quantile The matrix result of choose_quantile, with factor, quantile, FDR and TPR.
+#'@param FDR_goal The maximum FDR wanted.
+#'
+#'@return This function returns a list of 4 parameters : the best factor, the best quantile,
+#' and the associated FDR and TPR.
+#'
+#'@example examples/ex_select_quantile_param.R
+#'
+#'@export
+
+select_quantile_param = function(which_quantile, FDR_goal = 0.15){
+
+#Compute the small FDR.
+  small_FDR = which_quantile[which_quantile[,"FDR"] <= FDR_goal & !is.na(which_quantile[,"FDR"]) ,]
   if(length(small_FDR) !=0){
     if (is.vector(small_FDR)){
-      print ("The quantile closest to the FDR for this factor is")
-      print (small_FDR[1])
-      print("Which has a FDR for the naive method of")
-      print(small_FDR[2])
+      print ("Best parameters are :", quote = FALSE)
+      print (c(small_FDR[1], small_FDR[2], small_FDR[3], small_FDR[4]), quote = FALSE)
+
+      return(list(factor = small_FDR[1], quantile = small_FDR[2], FDR = small_FDR[3], TPR = small_FDR[4]))
+
     } else {
-      idx = which(small_FDR[,2] == max(small_FDR[,2]))
-      print ("The quantile closest to the FDR for this factor is")
-      print (small_FDR[idx, 1])
-      print("Which has a FDR for the naive method of")
-      print(small_FDR[idx, 2])
+      idx = which(small_FDR[,"TPR"] == max(small_FDR[,"TPR"]))
+      print ("Best parameters are : ", quote = FALSE)
+      print(c(small_FDR[idx, 1], small_FDR[idx, 2]
+               , small_FDR[idx, 3] , small_FDR[idx, 4]), quote = FALSE)
+
+      return(list(factor = small_FDR[idx, 1], quantile = small_FDR[idx, 2], FDR = small_FDR[idx, 3], TPR = small_FDR[idx, 4]))
     }
   } else {
     print ("Your FDR is not reachable, check the results table to choose your quantile.")
   }
-  colnames(results) = c("quantile", "FDR", "TPR", "FPR")
-  return(results)
 }
